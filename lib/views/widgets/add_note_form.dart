d54fd27b8da41c42ec4909d0d113520fde485de2
KhaledElalfy1 <khaledalfy53@gmail.com>
2023-07-30 07:44:28 -0700

    refactor block provider

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubit/add_note/add_note_cubit.dart';
+import 'package:note_app/models/note_model.dart';
 
 import 'custom_button.dart';
 import 'custom_textform.dart';
@@ -48,11 +51,15 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+                var note = NoteModel(
+                    title: title!,
+                    subTitle: subTitle!,
+                    date: DateTime.now().toString(),
+                    color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(note);
               } else {
                 autoValidateMode = AutovalidateMode.always;
-                setState(() {
-                  
-                });
+                setState(() {});
               }
             },
           ),
